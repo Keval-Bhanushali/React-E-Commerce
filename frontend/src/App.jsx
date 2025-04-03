7e8160fb6e4c5695f9a027399ce5df2ae2409d0a
Keval Bhanushali <mrbhanushali03@gmail.com>
2025-04-03 10:00:22 +0530

    work on ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/signup" element={<SignUpPage />} /> 
+        <Route path="/login" element={<LoginPage />} /> 
+      </Routes>
+    </div>
+  )
+}
+
+export default App
